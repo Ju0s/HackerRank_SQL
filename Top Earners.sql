19e5f2cc1483b9ac247bab655ec4078af23bb381
Jeong Juyoung <154944675+Ju0s@users.noreply.github.com>
2025-06-12 14:44:06 +0900

    250612

---
+++ Top Earners.sql
@@ -0,0 +1,7 @@
+-- employee's total earnings = salary * months
+-- maximum total earnings, total number of employees who have maximum total earnings
+-- print these values as 2 space-separated integers
+SELECT CONCAT(MAX(months * salary), '  ', COUNT(employee_id))
+FROM Employee
+WHERE months * salary = (SELECT MAX(months * salary)
+                         FROM Employee)
