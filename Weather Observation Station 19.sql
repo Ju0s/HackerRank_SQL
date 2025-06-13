7b9c8581c97da0bc6d388618f73c874a0d3ebacf
Jeong Juyoung <154944675+Ju0s@users.noreply.github.com>
2025-06-13 19:54:43 +0900

    250613

---
+++ Weather Observation Station 19.sql
@@ -0,0 +1,4 @@
+-- Euclidean Distance between points P1 and P2
+-- format to display 4 decimal digits
+SELECT TRUNCATE(SQRT(POWER(MIN(LAT_N) - MAX(LAT_N), 2) + POWER(MIN(LONG_W) - MAX(LONG_W), 2)), 4)
+FROM STATION;
